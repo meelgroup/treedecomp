3f96aeb75a365e1670e575940d811cc7fe8b816a
Mate Soos <soos.mate@gmail.com>
2026-03-06 22:07:46 +0100

    emscripten is static compile always

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -2,6 +2,6 @@
 set -e
 
 rm -rf .cmake CMake* src cmake* Make*
-emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF ..
+emcmake cmake -DSTATICCOMPILE=ON -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF ..
 emmake make -j26
 emmake make install
