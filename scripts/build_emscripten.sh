b70231272236ce9cdab9c8e5f48a200cc7d359d9
Mate Soos <soos.mate@gmail.com>
2026-04-03 16:32:07 +0200

    Fix build for staticcompile

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -2,6 +2,6 @@
 set -e
 
 rm -rf .cmake CMake* src cmake* Make*
-emcmake cmake -DSTATICCOMPILE=ON -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF ..
+emcmake cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF ..
 emmake make -j$(nproc)
 emmake make install
