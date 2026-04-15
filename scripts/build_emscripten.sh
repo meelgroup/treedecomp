e85d109fd92dc5635e88d130c48a57e5123fdadb
Mate Soos <soos.mate@gmail.com>
2026-04-15 13:57:53 +0200

    Better emscripten

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -6,6 +6,9 @@ rm -rf CMake*
 rm -rf src
 rm -rf cmake*
 rm -rf Make*
-emcmake cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF ..
+emcmake cmake -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_INSTALL_PREFIX=$EMINSTALL \
+    -DENABLE_TESTING=OFF ..
 emmake make -j$(nproc)
 emmake make install
